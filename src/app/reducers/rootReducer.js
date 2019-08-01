13f2906a6e8e92566d2fd7ba70c0e5897365d1e8
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-08-01 18:15:06 +0200

    Add async reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 /* Reducers */
 import testReducer from '../../features/testarea/testReducer';
+import asyncReducer from '../../features/async/asyncReducer';
 
 const rootReducer = combineReducers({
-  test: testReducer
-})
+  test: testReducer,
+  async: asyncReducer
+});
 
 export default rootReducer;
