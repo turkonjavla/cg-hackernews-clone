ccabca8efb2fd2478feaf14991daa2c41336508b
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-08-01 17:04:16 +0200

    Add redux store config

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,33 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers/rootReducer';
+
+export const configureStore = preloadedState => {
+  
+  // middlewares
+  const middlewares = [];
+  const middlewareEnchancer = applyMiddleware(...middlewares);
+
+  // enchancers
+  const storeEnchancers = [middlewareEnchancer];
+  const composedEnchancer = composeWithDevTools(...storeEnchancers);
+
+  // store
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    composedEnchancer
+  );
+
+  // hot module reload for redux
+  if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('../reducers/rootReducer', () => {
+        const newRootReducer = require('../reducers/rootReducer').default;
+        store.replaceReducer(newRootReducer);
+      })
+    }
+  }
+
+  return store;
+}
