2ecb37a84513df25394c5148877e56dcc7e6be00
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-08-01 12:01:38 +0200

    Add initial route and ArticleDashboard component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+/* React router */
+import { Route, Switch } from 'react-router-dom';
+
 /* MUI Components */
 import CSSBaseline from '@material-ui/core/CssBaseline/';
+
+/* Components */
+import ArticleDashboard from '../../features/article/ArticleDashboard/ArticleDashboard';
 
 const App = () => {
   return (
     <div className="App">
       <CSSBaseline />
-      <p>Hello</p>
+      <Switch>
+        <Route path="/" component={ArticleDashboard} />
+      </Switch>
     </div>
   );
 }
---
+++ src/features/article/ArticleDashboard/ArticleDashboard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ArticleDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Article Dashboard</h2>
+      </div>
+    )
+  }
+}
+
+export default ArticleDashboard;
