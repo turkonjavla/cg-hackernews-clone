3d6aff63ef91a44d73c4c04a57e47f43580b3012
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-08-01 14:14:59 +0200

    Add search field

--- src/features/article/ArticleDashboard/ArticleDashboard.jsx
+++ src/features/article/ArticleDashboard/ArticleDashboard.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 /* MUI Components */
 import withStyles from '@material-ui/core/styles/withStyles';
 import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 
 /* Components */
@@ -20,6 +21,16 @@ class ArticleDashboard extends Component {
     const { classes } = this.props;
     return (
       <Container maxWidth="lg">
+        <TextField
+          id="outlined-search"
+          label="Search front page stories"
+          className={classes.textField}
+          // value={values.search}
+          // onChange={handleChange('search')}
+          margin="normal"
+          variant="outlined"
+          fullWidth
+        />
         <Grid container spacing={4} className={classes.mainGrid}>
           <ArticleList />
         </Grid>
