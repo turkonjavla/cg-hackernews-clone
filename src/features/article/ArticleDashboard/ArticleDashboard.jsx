b22f63a61419974f68065aee9740090aab3592b8
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-08-01 13:57:22 +0200

    Create layout and components for displaying articles

--- src/features/article/ArticleDashboard/ArticleDashboard.jsx
+++ src/features/article/ArticleDashboard/ArticleDashboard.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/* MUI Components */
+import withStyles from '@material-ui/core/styles/withStyles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+/* Components */
+import ArticleList from '../ArticleList/ArticleList';
+
+const styles = theme => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  }
+});
 
 class ArticleDashboard extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h2>Article Dashboard</h2>
-      </div>
+      <Container maxWidth="lg">
+        <Grid container spacing={4} className={classes.mainGrid}>
+          <ArticleList />
+        </Grid>
+      </Container>
     )
   }
 }
 
-export default ArticleDashboard;
+ArticleDashboard.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(ArticleDashboard);
---
+++ src/features/article/ArticleList/ArticleList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* MUI Components */
+import ArticleListItem from './ArticleListItem';
+
+const ArticleList = ({ classes }) => {
+  return (
+    <ArticleListItem />
+  )
+}
+
+export default ArticleList;
