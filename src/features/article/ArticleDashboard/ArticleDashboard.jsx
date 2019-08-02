fedb513fe89ec5cfcdc9785763b5fecc2fc5d840
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-08-02 19:35:17 +0200

    Add options for sorting articles by title and date

--- src/features/article/ArticleDashboard/ArticleDashboard.jsx
+++ src/features/article/ArticleDashboard/ArticleDashboard.jsx
@@ -9,23 +9,40 @@ import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 /* Components */
 import ArticleList from '../ArticleList/ArticleList';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 /* Actions */
-import { loadArticles } from '../articleActions';
+import { loadArticles, sort } from '../articleActions';
+
+/* Selectors */
+import { getSortedArticlesSelector } from '../articleSelector';
+
 
 const styles = theme => ({
   mainGrid: {
     marginTop: theme.spacing(3),
-  }
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 });
 
 class ArticleDashboard extends Component {
   state = {
-    searchQuery: ''
+    searchQuery: '',
+    sortKey: '',
+    sortDirection: ''
   }
 
   componentDidMount() {
@@ -46,37 +63,103 @@ class ArticleDashboard extends Component {
     })
   }
 
+  // sorting by date
+  handleSortClick = () => {
+    const { sortDirection, sortKey } = this.state;
+    this.props.sort(sortDirection, sortKey);
+    this.setState({
+      searchQuery: ''
+    })
+  }
+
+  handleKeyChange = (e) => {
+    this.setState({
+      sortKey: e.target.value
+    });
+  }
+
+  handleDirectionChange = (e) => {
+    this.setState({
+      sortDirection: e.target.value
+    });
+  }
+
   render() {
     const { classes, loading, articles } = this.props;
-    const { searchQuery } = this.state;
+    const { searchQuery, sortKey, sortDirection } = this.state;
 
     if (loading) return <LoadingComponent />
 
     return (
       <Container maxWidth="lg">
-        <TextField
-          label="Search front page stories"
-          name="searchQuery"
-          onKeyUp={this.handleInputChange}
-          margin="normal"
-          variant="outlined"
-          fullWidth
-        />
-        <Grid container spacing={4} className={classes.mainGrid}>
-          <Button
-            onClick={this.refreshData}
-            disabled={loading}
-            fullWidth
+        <Grid container>
+
+          {/* search field */}
+          <TextField
+            label="Search front page stories"
+            name="searchQuery"
+            onKeyUp={this.handleInputChange}
+            margin="normal"
             variant="outlined"
-          >
-            Refresh Data
-          </Button>
+            fullWidth
+          />
+
+          {/* sort by field */}
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="sortKey">Sort by</InputLabel>
+            <Select
+              value={sortKey}
+              onChange={this.handleKeyChange}
+              inputProps={{
+                name: 'sortKey',
+                id: 'sortKey',
+              }}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="created_at">Date</MenuItem>
+              <MenuItem value="title">Title</MenuItem>
+            </Select>
+          </FormControl>
+
+          {/* sort direction field */}
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="sortDirection">Direction</InputLabel>
+            <Select
+              value={sortDirection}
+              onChange={this.handleDirectionChange}
+              inputProps={{
+                name: 'sortDirection',
+                id: 'sortDirection',
+              }}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="desc">Descending</MenuItem>
+              <MenuItem value="asc">Ascending</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Button variant="outlined" onClick={this.handleSortClick}>Apply Filter</Button>
+        <Grid container spacing={4} className={classes.mainGrid}>
+          <Grid item>
+            <Button
+              onClick={this.refreshData}
+              disabled={loading}
+              fullWidth
+              variant="outlined"
+            >
+              Refresh Data
+            </Button>
+          </Grid>
           <ArticleList
             articles={articles}
             searchQuery={searchQuery}
           />
         </Grid>
-      </Container>
+      </Container >
     )
   }
 }
@@ -84,16 +167,21 @@ class ArticleDashboard extends Component {
 ArticleDashboard.propTypes = {
   classes: PropTypes.object.isRequired,
   loading: PropTypes.bool.isRequired,
-  articles: PropTypes.array
+  articles: PropTypes.array,
+  loadArticles: PropTypes.func.isRequired,
+  sort: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
   loading: state.async.loading,
-  articles: state.articles.hits
+  articles: getSortedArticlesSelector(state),
+  sortKey: state.articles.sortKey,
+  sortDirection: state.articles.sortDirection
 });
 
 const actions = {
-  loadArticles
+  loadArticles,
+  sort
 }
 
 export default compose(
